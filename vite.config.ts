2fd077b1528195307c260d0f9548e46b5eae3283
Nicolas <nicolas@MacBookPro.lan>
2025-01-20 12:06:24 +0200

    chore: install svgr plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,15 @@ export default defineConfig({
       'components-library': path.resolve(__dirname, 'node_modules/components-library/dist/components-library'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(), 
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          floatPrecision: 2,
+        },
+      },
+    })
+  ],
 })
