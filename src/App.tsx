7e0a523ee9fb148bf90e40b78843af1ffac112f6
Nicolas <nicolas@MacBookPro.lan>
2025-02-27 15:24:16 +0200

    refactor: use arrow function and move header to components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import Footer from "./components/Footer";
-import Header from "./components/Layout/Header";
+import { Box, Text } from "components-library";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
 import "components-library";
-export default function App() {
-  return (
-    <>
-      <Header />
-      <div className="pt-40 pb-20 flex items-center justify-center bg-gray-100">
-        <h1 className="h-screen text-4xl font-bold text-green-600">
-          Hello, Tailwind!
-        </h1>
-      </div>
-      <Footer />
-    </>
-  );
-}
+
+const App = () => (
+  <>
+    <Header />
+    <Box className="pt-40 pb-20 flex items-center justify-center bg-gray-100">
+      <Text as="h1" className="h-screen text-4xl font-bold text-green-600">
+        Hello, Tailwind!
+      </Text>
+    </Box>
+    <Footer />
+  </>
+);
+
+export default App;
